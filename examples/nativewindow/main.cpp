f0b776cb57f2de501b8b0602a61837c10444caca
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2016-12-06 10:06:55 +0100

    Add test example 'nativewindow'

---
+++ examples/nativewindow/main.cpp
@@ -0,0 +1,10 @@
+#include <QtGui>
+#include <QtQml>
+#include <QtQuick>
+
+int main(int argc, char **argv){
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine(QUrl("qrc:///main.qml"));
+    return app.exec();
+}
+
---
+++ examples/nativewindow/main.qml
@@ -0,0 +1,8 @@
+import QtQuick 2.1
+import Qt.NativeControls 1.0 as Native
+
+Native.Window {
+    width: 480
+    height: 640
+    visible: true
+}
