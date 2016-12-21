9e7abca31e3a98572213dd9a21c8d3343d23f1fb
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2016-12-21 14:06:02 +0100

    example: uikit: hybrid: make more robust

--- examples/cpp/platforms/uikit/hybrid/main.mm
+++ examples/cpp/platforms/uikit/hybrid/main.mm
@@ -10,16 +10,19 @@ int main(int argc, char *argv[])
     QNativeWindow window;
     QNativeButton nativeButton(QStringLiteral("QNativeButton"), &window);
     nativeButton.setGeometry(10, 10, 100, 30);
-    QObject::connect(&nativeButton, &QNativeButton::clicked, [&nativeButton](){ nativeButton.setText(QStringLiteral("Clicked!")); });
+    QObject::connect(&nativeButton, &QNativeButton::clicked,
+                     [&nativeButton](){ nativeButton.setText(QStringLiteral("Clicked!")); });
 
-    QNativeUIKitWindow *nativeUIKitWindow = dynamic_cast<QNativeUIKitWindow *>(window.platformHandle());
-    QNativeUIKitButton nativeUIKitButton(QStringLiteral("QNativeUIKitButton"), nativeUIKitWindow);
-    nativeUIKitButton.setGeometry(10, 50, 100, 30);
-    QObject::connect(&nativeUIKitButton, &QNativeUIKitButton::clicked, [&nativeUIKitButton](){ nativeUIKitButton.setText(QStringLiteral("Clicked!")); });
+    if (QNativeUIKitWindow *nativeUIKitWindow = dynamic_cast<QNativeUIKitWindow *>(window.platformHandle())) {
+        QNativeUIKitButton *nativeUIKitButton = new QNativeUIKitButton(nativeUIKitWindow);
+        nativeUIKitButton->setGeometry(10, 50, 100, 30);
+        QObject::connect(nativeUIKitButton, &QNativeUIKitButton::clicked,
+                         [&nativeUIKitButton](){ nativeUIKitButton->setText(QStringLiteral("Clicked!")); });
 
-    UIButton *uiButton = nativeUIKitButton.uiButtonHandle();
-    uiButton.tintColor = [UIColor redColor];
-    [uiButton setTitle:@"Yup" forState:UIControlStateNormal];
+        UIButton *uiButton = nativeUIKitButton->uiButtonHandle();
+        [uiButton setTitle:@"Click me" forState:UIControlStateNormal];
+        uiButton.tintColor = [UIColor redColor];
+    }
 
     window.showFullScreen();
     return app.exec();
