eafd3c2e8b69584250d1ab5dd6391978dc37a089
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2017-05-03 13:47:48 +0200

    Imports, uikit: add support for labels

--- src/imports/platforms/uikit/qmluniuikitcontrolsplugin.cpp
+++ src/imports/platforms/uikit/qmluniuikitcontrolsplugin.cpp
@@ -62,6 +62,7 @@ void registerTypes(const char *uri) override
     qmlRegisterType<QUniUIKitButton>(uri, 1, 0, "Button");
     qmlRegisterType<QUniUIKitTextField>(uri, 1, 0, "TextField");
     qmlRegisterType<QUniUIKitSearchField>(uri, 1, 0, "SearchField");
+    qmlRegisterType<QUniUIKitLabel>(uri, 1, 0, "Label");
     qmlRegisterType<QUniUIKitSlider>(uri, 1, 0, "Slider");
     qmlRegisterType<QUniUIKitTableView>(uri, 1, 0, "TableView");
     qmlRegisterType<QUniUIKitTableViewCell>(uri, 1, 0, "TableViewCell");
